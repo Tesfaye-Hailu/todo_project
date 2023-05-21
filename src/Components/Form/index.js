16daf137152fe20d819f4d648bf12ff6566e8190
Tesfaye Hailu <tes.girmah@gmail.com>
2023-05-20 21:49:25 -0700

    first commit

---
+++ src/Components/Form/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Form = () => {
+  return (
+    <div>
+        <Form>
+            <input placeholder="Add Todo Item"></input>
+            <button>Add</button>
+        </Form>
+
+    </div>
+  )
+}
+
+export default Form
---
+++ src/Components/Header/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import styles from "./Style";
+const Header = () => {
+  return (
+    <div className={styles.header}>Todo App</div>
+  )
+}
+
+export default Header;
